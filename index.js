334880f166ef891c4caa396a35853d3c4afeb188
Aingaran <eaingaran@gmail.com>
2019-05-15 02:34:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function typingEffect() {
 			deletingEffect();
 			return false;
 		};
-		timer = setTimeout(loopTyping, 500);
+		timer = setTimeout(loopTyping, 300);
 	};
 	loopTyping();
 };
@@ -31,7 +31,7 @@ function deletingEffect() {
 			typingEffect();
 			return false;
 		};
-		timer = setTimeout(loopDeleting, 200);
+		timer = setTimeout(loopDeleting, 150);
 	};
 	loopDeleting();
 };
