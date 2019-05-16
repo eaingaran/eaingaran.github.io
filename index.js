8e598730a87886c62cdf896fb15a819ab6a7c0ce
Aingaran <eaingaran@gmail.com>
2019-05-16 22:10:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+function copyEmail() {
+   // Create new element
+   var el = document.createElement('textarea');
+   // Set value (string to be copied)
+   el.value = 'me@aingaran.dev';
+   // Set non-editable to avoid focus and move outside of view
+   el.setAttribute('readonly', '');
+   el.style = {position: 'absolute', left: '-9999px'};
+   document.body.appendChild(el);
+   // Select text inside element
+   el.select();
+   // Copy text to clipboard
+   document.execCommand('copy');
+   // Remove temporary element
+   document.body.removeChild(el);
+     // Get the snackbar DIV
+  var x = document.getElementById("snackbar");
+
+  // Add the "show" class to DIV
+  x.className = "show";
+
+  // After 3 seconds, remove the show class from DIV
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
+
+
 function switchToAbout()  {
   document.getElementById("home").style.display = 'none';
   document.getElementById("aboutme").style.display = 'block';
