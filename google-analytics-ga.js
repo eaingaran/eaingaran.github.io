401646956c6cb3c1857db4eea54263c12b42c32f
Aingaran <eaingaran@gmail.com>
2023-03-08 10:32:05 +0100

    feat: add google tag

--- google-analytics-ga.js
+++
@@ -1,8 +0,0 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-EC00PJEK0C"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-EC00PJEK0C');
-</script>
