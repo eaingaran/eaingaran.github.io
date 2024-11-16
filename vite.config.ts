ae9ca6258fe0884d30161f97f9cc040b6614e566
Aingaran Elango <me@aingaran.dev>
2024-11-16 09:58:17 +0100

    fix: attempt to fix server side routing (GitHub pages are not designed for dynamic websites)

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tsconfigPaths(), react()],
   resolve: {
     alias: {
-      '*': './src/*',
+      "*": "./src/*",
     },
   },
-})
+  base: "/",
+});
